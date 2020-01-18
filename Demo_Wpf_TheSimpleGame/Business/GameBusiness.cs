e3a9b3e0e0a63be6c2c3907f1f151ef044c6f79f
johnvelis <john@johnvelis.com>
2020-01-18 10:50:27 -0500

    Add project files.

---
+++ Demo_Wpf_TheSimpleGame/Business/GameBusiness.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo_Wpf_TheSimpleGame.Presentation;
+using Demo_Wpf_TheSimpleGame.Models;
+
+namespace Demo_Wpf_TheSimpleGame.Business
+{
+    /// <summary>
+    /// business logic layer class
+    /// manages windows and interacts with the data layer
+    /// </summary>
+    public class GameBusiness
+    {
+        public GameBusiness()
+        {
+            GameView gameView = new GameView();
+            GameViewModel gameViewModel = new GameViewModel(gameView);
+
+
+        }
+    }
+}
---
+++ Demo_Wpf_TheSimpleGame/Presentation/GameViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Wpf_TheSimpleGame.Presentation
+{
+    class GameViewModel
+    {
+        private GameView gameView;
+
+        public GameViewModel(GameView gameView)
+        {
+            this.gameView = gameView;
+        }
+    }
+}
